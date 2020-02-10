221552dadbef424dfe99ba827b0e23cbd1ebdf37
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-09 22:13:20 -0500

    Started Number Wizard Project with Console Greeting

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to the number wizard.");
+        Debug.Log("Pick a number between...");
+        Debug.Log("Highest number: "+max);
+        Debug.Log("And Lowest Number: "+min);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
