aee346cbf5ca8b8096d2bd1076fe04158d6cfb8f
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-12 14:22:34 -0500

    Created base UI and programmed story text reference

---
+++ Pick a Path/Assets/PickAPath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickAPath : MonoBehaviour
+{
+    //SerializeField makes this variable visible within the game object inspector in Unity UI
+    //in this case it is used to assign the story text block to this component from the UI
+    [SerializeField] Text textComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //text within the textbox within the text component
+        textComponent.text = "Here the story begins...";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
