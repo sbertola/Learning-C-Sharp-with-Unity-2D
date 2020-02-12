887f51f64425a9f0c0ed99c0477af9023f5a14f9
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-12 14:57:49 -0500

    Created story beginning and scriptable state object

--- Pick a Path/Assets/PickAPath.cs
+++ Pick a Path/Assets/PickAPath.cs
@@ -14,7 +14,11 @@ public class PickAPath : MonoBehaviour
     void Start()
     {
         //text within the textbox within the text component
-        textComponent.text = "Here the story begins...";
+        textComponent.text = "You suddenly awaken to the sounds of screaming and metal clashing." +
+            "'The village must be under attack!' you think to yourself. It's time to act quickly, will you...\n\n" +
+            "1.Head towards the back of your house towards the cellar?\n" +
+            "2.Head towards the front of your house to get a better view of the situation?\n" +
+            "3.Pull the blankets over your head and attempt to fall back to sleep?";
     }
 
     // Update is called once per frame
---
+++ Pick a Path/Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Note: scriptible objects like this onedon't have to be connected to a game object
+//State is a ScriptableObject class instead of a Monobehaviour one
+//createAssetMenu adds the ability to right and create a 'State' asset from the menu
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    //TextArea defines inspector textbox in unity UI, 10 here is the minimum size of the textfield and 
+    //the second number 14 is the minimum number of lines before a scroll bar appears
+    [TextArea(10,14)] [SerializeField] string storyText;
+}
