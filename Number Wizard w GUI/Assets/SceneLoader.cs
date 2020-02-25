198df6afb845a4e8913b21ce50b3424cacda690f
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-24 21:04:39 -0500

    Added game loop and core game image details

--- Number Wizard w GUI/Assets/SceneLoader.cs
+++ Number Wizard w GUI/Assets/SceneLoader.cs
@@ -11,4 +11,9 @@ public class SceneLoader : MonoBehaviour
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
