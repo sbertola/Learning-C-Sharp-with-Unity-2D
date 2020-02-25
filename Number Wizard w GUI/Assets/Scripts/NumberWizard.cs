36101488dd04493cc7751a43afeab3d78a56a9df
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-24 21:42:39 -0500

    Complete build for runnable executible

---
+++ Number Wizard w GUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max = 1000;
+    [SerializeField] int min = 1;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess = 500;
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess +1;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess+1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min,max+1);
+        guessText.text = guess.ToString();
+    }
+}
