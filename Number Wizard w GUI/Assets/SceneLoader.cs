5b3814d134aa7e011f88909d8c1d83fb022bfe5b
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-24 20:48:03 -0500

    Added Scene Transition from start to game

---
+++ Number Wizard w GUI/Assets/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
