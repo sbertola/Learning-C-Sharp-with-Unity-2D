eea5fce3024cf3a9b223297fc957de3a6c196f4f
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-26 14:12:51 -0500

    Added block collsion physics

---
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        //can use the collision parameter to get additional information from the collision.
+    }
+}
