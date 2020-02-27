e228f3b278eb826c09852b9927a1ade3fe949b27
Sebastian Bertola <sbertola@uoguelph.ca>
2020-02-26 21:01:50 -0500

    Added game loop and fixed bug where level didn't load properly

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        DestryBlock();
         Destroy(gameObject);
         //can use the collision parameter to get additional information from the collision.
+    }
+
+    private void DestryBlock()
+    {
+        level.BlockDestroyed();
     }
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //serialized for debugging
+    [SerializeField] int breakableBlocks;
+
+    //caced referenceto avoid multiple references.
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+ 
+}
