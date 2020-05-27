281713d2b9b40680297f918d1a549c422d73bdd9
Sebastian Bertola <sbertola@uoguelph.ca>
2020-05-26 22:16:28 -0400

    Added player shooting

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -1,20 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //configuration parameters
     [SerializeField] float movespeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundries();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuosly());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+
+    }
+
+    IEnumerator FireContinuosly()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 
     private void Move()
@@ -22,8 +62,21 @@ public class Player : MonoBehaviour
         //GetAxis allows using a variety of keys to get input for the same command, refer to input panel in Edit>Project settings
         var deltaX = Input.GetAxis("Horizontal")*Time.deltaTime*movespeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX,xMin,xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY,yMin,yMax);
         transform.position = new Vector2(newXPos, newYPos); //transform refers to the instance, set new x and y
+    }
+
+    private void SetUpMoveBoundries()
+    {
+        Camera gameCamera = Camera.main;
+
+        //used to determin position relative to camera view, where (1,1) = top righ of camera
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
