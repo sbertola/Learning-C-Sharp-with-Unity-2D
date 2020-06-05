eb7dbdd209e17549e9bc089273cadf56c173b74a
Sebastian Bertola <sbertola@uoguelph.ca>
2020-06-04 23:14:29 -0400

    Added some enemy pathing

---
+++ Space Shooter/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] float moveSpeed = 2f;
+
+    int wayPointIndex = 0;
+    List<Transform> waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //MoveTowards function used to travel across a set of waypoints
+    private void Move()
+    {
+        if (wayPointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Space Shooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEniemies(); enemyCount++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+        
+    }
+
+}
